f22e817697b81b7d6f7960cf315da6230fd7f610
Benny Goh <haiguan648@gmail.com>
2023-01-31 13:03:34 +0800

    Fixed first click bug + added concatenate function

--- RandomNameGenerator/src/generateName.js
+++ RandomNameGenerator/src/generateName.js
@@ -2,14 +2,15 @@ const generate = require('project-name-generator')
 const answer = document.querySelector('.answer')
 const dashed = document.getElementById('dashed')
 const spaced = document.getElementById('spaced')
+const concatenate = document.getElementById('concatenate')
 const number = document.getElementById('number')
 const alliterative = document.getElementById('alliterative')
 const numofwords = document.getElementById('numofwords')
-const nameBtn = document.querySelector('.nameBtn')
 
 function generateName() {
     let dashedValid = dashed.checked;
     let spacedValid = spaced.checked;
+    let concatenatevalid = concatenate.checked;
     let numberValid = number.checked;
     let alliterativeValid = alliterative.checked;
     let numberofwords = parseInt(numofwords.value);
@@ -18,26 +19,30 @@ function generateName() {
 
     if(dashedValid) {
         mode = 'dashed'
-    } else if (!dashedValid) {
-        mode = 'spaced'
     } else if (spacedValid) {
         mode = 'spaced'
-    } else if (!spacedValid) {
-        mode = 'dashed'
+    } else if (concatenatevalid) {
+        mode = 'concatenate'
     }
 
-    nameBtn.addEventListener('click', () => {
-        let options = {words: numberofwords};
-        if (numberValid) {
-            options.number = true;
-        }
-        if (alliterativeValid) {
-            options.alliterative = true;
-        }
-        answer.innerHTML = generate(options)[mode];
-    });
-}
+    let options = {words: numberofwords};
+    if (numberValid) {
+        options.number = true;
+    }
+    if (alliterativeValid) {
+        options.alliterative = true;
+    }
 
-generateName()
+    if (concatenatevalid) {
+        let words = generate(options)['spaced'].split(" ");
+        let result = "";
+        for (let word of words) {
+          result += word[0].toUpperCase() + word.slice(1);
+        }
+        answer.innerHTML = result;
+      } else {
+        answer.innerHTML = generate(options)[mode];
+      }
+    }      
 
 export default generateName
